9a726fd94ed2393b9425df8e95ce6ec6885a8682
julianrodriguez77 <60676300+julianrodriguez77@users.noreply.github.com>
2022-07-19 23:40:21 -0500

    Update Entrenamiento.py

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -2,24 +2,24 @@ import cv2
 import os
 import numpy as np
 
-DirData = 'C:/Users/JULIAN/Documents/PYTHON/DetectorAnimal/imagenes'
-ListaAnimal = os.listdir(DirData)
-print('Lista de Animales: ', ListaAnimal)
+DirData = 'C:/Users/JULIAN/Documents/PYTHON/Detector/imagenes'
+ListaFace = os.listdir(DirData)
+print('Lista de Face: ', ListaFace)
 #conteos
 labels = []
 rostrosData = []
 label = 0
 
-for nameDir in ListaAnimal:
-	AnimalPath = DirData + '/' + nameDir
+for nameDir in ListaFace:
+	FacePath = DirData + '/' + nameDir
 	print('Leyendo las imágenes')
 
-	for fileName in os.listdir(AnimalPath):
+	for fileName in os.listdir(FacePath):
 		print('Rostros: ', nameDir + '/' + fileName)
 		labels.append(label) #variable contadora
 
-		rostrosData.append(cv2.imread(AnimalPath + '/' + fileName, 0))
-		img = cv2.imread(AnimalPath+'/'+fileName,0)
+		rostrosData.append(cv2.imread(FacePath + '/' + fileName, 0))
+		img = cv2.imread(FacePath+'/'+fileName,0)
 		#cv2.imshow('img',img) #para ver si funciona
 		#cv2.waitKey(10)
 	label = label + 1 # variable de conteo
@@ -33,5 +33,5 @@ for nameDir in ListaAnimal:
 face_recognizer = cv2.face.LBPHFaceRecognizer_create() #modelos para entrenar
 print("Se esta realizando el entrenamiento....")
 face_recognizer.train(rostrosData, np.array(labels))
-face_recognizer.write('modeloLBPHFaceanimal.xml') # nuestro modelo
+face_recognizer.write('modeloLBPHFace.xml') # nuestro modelo
 print("El modelo se a guardado")
