699a838d3ef9d29ca010cad18f9cb7977126f523
julianrodriguez77 <60676300+julianrodriguez77@users.noreply.github.com>
2022-07-19 23:33:12 -0500

    Update animaldetectorface.py

--- animaldetectorface.py
+++ animaldetectorface.py
@@ -1,12 +1,12 @@
 import cv2
 import os
 
-DirData = 'C:/Users/JULIAN/Documents/PYTHON/DetectorAnimal/imagenes'
+DirData = 'C:/Users/JULIAN/Documents/PYTHON/Detectorface/imagenes' #ubicacion de la carpeta donde se estan las imagenes
 imgPaths = os.listdir(DirData)
 print('imgPath', imgPaths)
 
 face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-face_recognizer.read('modeloLBPHFaceanimal.xml') # nombre del modelo guardado xml
+face_recognizer.read('modeloLBPHFace.xml') # nombre del modelo guardado xml
 
 #configuramos la camara
 camara = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -28,11 +28,11 @@ while True:
 
         cv2.putText(x2, '{}'.format(prediccion), (x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA) # color de la letras de prediccion
 
-        if prediccion[1] < 90: #para determinar si es el animal que esta en la base
+        if prediccion[1] < 90: #para determinar si es el rostro que esta en la base
             cv2.putText(x2, '{}'.format(imgPaths[prediccion[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
             cv2.rectangle(x2, (x,y), (x+w, y+h), (0,255,0),2)
         else:
-            cv2.putText(x2,'Animal Desconocido', (x, y-20), 2, 0.8, (0,0,255),1, cv2.LINE_AA)
+            cv2.putText(x2,'Rostro Desconocido', (x, y-20), 2, 0.8, (0,0,255),1, cv2.LINE_AA)
             cv2.rectangle(x2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('x2', x2)
